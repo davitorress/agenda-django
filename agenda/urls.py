4644b527cb75a657f11c36985a3b6a276f12175b
davitorress <dev.davitorres@gmail.com>
2022-07-29 15:57:06 -0300

    Incluir tela de autenticação

--- agenda/urls.py
+++ agenda/urls.py
@@ -21,5 +21,10 @@ from core import views
 urlpatterns = [
     path('', RedirectView.as_view(url='/agenda/')),
     path('admin/', admin.site.urls),
+
+    path('login/', views.login_user),
+    path('login/submit', views.submit_login),
+    path('logout/', views.logout_user),
+
     path('agenda/', views.lista_eventos),
 ]
--- core/views.py
+++ core/views.py
@@ -1,10 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from core.models import Evento
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
+def login_user(request):
+    return render(request, 'login.html')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('/')
+
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, "Usuário e/ou senha inválidos!")
+    return redirect('/')
+
+
+@login_required(login_url='/login/')
 def lista_eventos(request):
     usuario = request.user
-    evento = Evento.objects.all()
-    # evento = Evento.objects.filter(usuario=usuario)
+    evento = Evento.objects.filter(usuario=usuario)
     response = {'eventos': evento}
     return render(request, 'agenda.html', response)
